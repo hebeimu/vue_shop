614412887d784160afad4178c49a517e33e416f0
何北木 <583131099@qq.com>
2020-09-18 15:07:03 +0800

    初始化

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+      hah
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
+
+<style scoped>
+  .login {
+    background-color: #2b4b6b;
+    height: 100%;
+  }
+ 
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 引入全局的 css 样式
+
+
 new Vue({
   router,
   render: h => h(App)
